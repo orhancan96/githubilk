f6cd379280b7db7740db57fa78bed37a6e01de56
orhancan96 <orhancankullac96@gmail.com>
2022-09-21 19:20:23 +0300

    hello

---
+++ src/javaHelloWorld/Main.java
@@ -0,0 +1,12 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello Java");
+        System.out.println("hello Java 2");
+        
+	}
+
+}
