376791d218aa3ded0441ddaf014a7fe33f100477
orhancan96 <113685640+orhancan96@users.noreply.github.com>
2022-09-24 14:31:25 +0300

    Add files via upload

---
+++ src/conditionals/Main.java
@@ -0,0 +1,20 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi=20;
+		if (sayi<20) {
+			System.out.println("Sayı yetersiz");
+		}else if (sayi==20) {
+			System.out.println("Sayı yeterli sayılır");
+		}else {
+			System.out.println("Sayı yeterli");
+		}
+		
+		
+
+	}
+
+}
